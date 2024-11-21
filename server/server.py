81a24de33c979f9114e40613552b1088e8df3b2d
Musaddik <musaddikmoulavi1808@gmail.com>
2024-11-21 18:26:14 +0530

    Initial commit with large files

--- server/server.py
+++ server/server.py
@@ -1,189 +1,67 @@
-# import pickle
-# import json
-# import numpy as np
-# import os
-# # from sklearn.exceptions import InconsistentVersionWarning
-# import warnings
-
-# __models = {}
-# __data_columns = {}
-# __locations = {}
-
-# def get_estimated_price(city, location, area_sqft, bedrooms, bathrooms):
-#     try:
-#         loc_index = __data_columns[city].index(location.lower())
-#     except ValueError:
-#         loc_index = -1
-
-#     x = np.zeros(len(__data_columns[city]))
-#     x[0] = area_sqft
-#     x[1] = bathrooms
-#     x[2] = bedrooms
-#     if loc_index >= 0:
-#         x[loc_index] = 1
-
-#     return round(__models[city].predict([x])[0], 2)
-
-# def load_saved_artifacts():
-#     print("Loading saved artifacts...start")
-#     global __models
-#     global __data_columns
-#     global __locations
-
-#     artifacts_path = "./artifacts"
-#     for city in os.listdir(artifacts_path):
-#         city_path = os.path.join(artifacts_path, city)
-#         if os.path.isdir(city_path):
-#             with open(os.path.join(city_path, "columns.json"), "r") as f:
-#                 __data_columns[city] = json.load(f)['data_columns']
-#                 __locations[city] = __data_columns[city][3:]  # first 3 columns are area_sqft, bathroomsrooms, bedrooms
-
-#             with warnings.catch_warnings():
-#                 warnings.simplefilter("ignore", category=InconsistentVersionWarning)
-#                 with open(os.path.join(city_path, "model.pickle"), 'rb') as f:
-#                     __models[city] = pickle.load(f)
-#     print("Loading saved artifacts...done")
-
-# def get_location_names(city):
-#     return __locations.get(city, [])
-
-# def get_data_columns(city):
-#     return __data_columns.get(city, [])
-
-# if __name__ == '__main__':
-#     load_saved_artifacts()
-
-
-
-# server.py
-
+# Import necessary libraries and modules
 from flask import Flask, request, jsonify, send_from_directory
-import util
+import util  # A custom module that we assume contains utility functions for the app
 import os
-from flask_cors import CORS
+from flask_cors import CORS  # This helps to handle Cross-Origin Resource Sharing (CORS)
 
+# Create a new Flask web application
 app = Flask(__name__, static_folder='../client', static_url_path='', template_folder='../client')
+
+# Allow CORS so that the frontend can communicate with this server from different origins
 CORS(app)
 
+# Define a route for the root URL ('/')
 @app.route('/')
 def home():
+    # When someone accesses the root URL, serve the 'index.html' file from the client directory
     return send_from_directory(app.static_folder, 'index.html')
 
+# Define a route to get location names based on a city
 @app.route('/get_location_names/<city>', methods=['GET'])
 def get_location_names(city):
+    # Call a function from the 'util' module to get location names for the given city
+    locations = util.get_location_names(city)
+    
+    # Create a JSON response containing the locations
     response = jsonify({
-        'locations': util.get_location_names(city)
+        'locations': locations
     })
+    
+    # Add a header to allow cross-origin requests (CORS)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+# Define a route to predict home prices based on various parameters
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
+    # Get the data sent in the POST request as JSON
     data = request.get_json()
+    
+    # Extract the data from the JSON request
     city = data['city']
     area_sqft = float(data['area_sqft'])
     location = data['location']
     bedrooms = int(data['bedrooms'])
     bathrooms = int(data['bathrooms'])
-
+    
+    # Call a function from the 'util' module to get the estimated price based on the input data
+    estimated_price = util.get_estimated_price(city, location, area_sqft, bedrooms, bathrooms)
+    
+    # Create a JSON response with the estimated price
     response = jsonify({
-        'estimated_price': util.get_estimated_price(city, location, area_sqft, bedrooms, bathrooms)
+        'estimated_price': estimated_price
     })
+    
+    # Add a header to allow cross-origin requests (CORS)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+# Start the Flask web server when this script is executed directly
 if __name__ == '__main__':
     print("Starting Python Flask Server For Home Price Prediction...")
+    
+    # Load any necessary saved artifacts or models (e.g., machine learning models)
     util.load_saved_artifacts()
+    
+    # Run the Flask app on all available IP addresses (0.0.0.0) and port 8000 in debug mode
     app.run(host='0.0.0.0', port=8000, debug=True)
-
-
-
-
-
-
-
-# from flask import Flask, request, jsonify, send_from_directory
-# import util
-# from flask_cors import CORS
-
-# app = Flask(__name__, static_folder='../client', static_url_path='', template_folder='../client')
-
-# # app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/')
-# def home():
-#     return send_from_directory(app.static_folder, 'index.html')
-
-
-# @app.route('/get_location_names/<city>', methods=['GET'])
-# def get_location_names(city):
-#     response = jsonify({
-#         'locations': util.get_location_names(city)
-#     })
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-# @app.route('/predict_home_price', methods=['POST'])
-# def predict_home_price():
-#     data = request.get_json()
-#     city = data['city']
-#     area_sqft = float(data['area_sqft'])
-#     location = data['location']
-#     bedrooms = int(data['bedrooms'])
-#     bathrooms = int(data['bathrooms'])
-
-#     response = jsonify({
-#         'estimated_price': util.get_estimated_price(city, location, area_sqft, bedrooms, bathrooms)
-#     })
-#     print(data)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-
-# if __name__ == '__main__':
-#     print("Starting Python Flask Server For Home Price Prediction...")
-#     util.load_saved_artifacts()
-#     # app.run(host='0.0.0.0')
-#     app.run()    
-
-# if __name__ == '__main__':
-#     print("Starting Python Flask Server For Home Price Prediction...")
-#     util.load_saved_artifacts()
-#     app.run(debug=True)
-
-
-
-# # trial
-# from flask import Flask, request, jsonify
-# import util
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Welcome to the Home Price Prediction API"
-
-# @app.route('/get_location_names/<city>', methods=['GET'])
-# def get_location_names(city):
-#     response = jsonify({
-#         'locations': util.get_location_names(city)
-#     })
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-# @app.route('/predict_home_price/<city>', methods=['POST'])
-# def predict_home_price(city):
-#     total_sqft = float(request.form['total_sqft'])
-#     location = request.form['location']
-#     bhk = int(request.form['bhk'])
-#     bath = int(request.form['bath'])
-
-#     response = jsonify({
-#         'estimated_price': util.get_estimated_price(city, location, total_sqft, bhk, bath)
-#     })
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-
--- server/util.py
+++ server/util.py
@@ -3,54 +3,105 @@ import json
 import numpy as np
 import os
 
+# Dictionaries to store models, data columns, and location names
 __models = {}
 __data_columns = {}
 __locations = {}
 
 def get_estimated_price(city, location, area_sqft, bedrooms, bathrooms):
+    """
+    Estimating the price of a home based on input parameters.
+
+    Parameters:
+    - city: The city where the home is located.
+    - location: Specific location or neighborhood within the city.
+    - area_sqft: Area of the home in square feet.
+    - bedrooms: Number of bedrooms in the home.
+    - bathrooms: Number of bathrooms in the home.
+
+    Returns:
+    - The estimated price of the home rounded to 2 decimal places.
+    """
     try:
+        # Finding the index of the location in the data columns for the city
         loc_index = __data_columns[city].index(location.lower())
     except:
+        # If the location is not found, set index to -1
         loc_index = -1
 
+    # Creating a feature vector with zeros
     x = np.zeros(len(__data_columns[city]))
+    
+    # Setting the features in the vector
     x[0] = area_sqft
     x[1] = bathrooms
     x[2] = bedrooms
+    
+    # If the location was found, set the corresponding feature to 1
     if loc_index >= 0:
         x[loc_index] = 1
- 
-
+    
+    # Using the model to predict the price based on the feature vector
     return round(__models[city].predict([x])[0], 2)
 
 def load_saved_artifacts():
+    """
+    Load saved models and data columns from files.
+    """
     print("Loading saved artifacts...start")
     global __models
     global __data_columns
     global __locations
 
+    # Path to the directory where artifacts are stored
     artifacts_path = "./artifacts"
+    
+    # Looping through each city's folder in the artifacts directory
     for city in os.listdir(artifacts_path):
         city_path = os.path.join(artifacts_path, city)
+        
+        # Checking if the path is a directory
         if os.path.isdir(city_path):
+            # Loading the data columns from JSON file
             with open(os.path.join(city_path, "columns.json"), "r") as f:
                 __data_columns[city] = json.load(f)['data_columns']
-                __locations[city] = __data_columns[city][3:]  # first 3 columns are area_sqft, bathroomsrooms, bedrooms
+                __locations[city] = __data_columns[city][3:]  # Get location names (columns after the first 3)
+            
+            # Load the model from pickle file
             with open(os.path.join(city_path, "model.pickle"), 'rb') as f:
-                __models[city] = pickle.load(f)            
+                __models[city] = pickle.load(f)
+    
     print("Loading saved artifacts...done")
 
 def get_location_names(city):
+    """
+    Getting a list of location names for a given city.
+
+    Parameters:
+    - city: The city for which to retrieve location names.
+
+    Returns:
+    - A list of location names for the city.
+    """
     return __locations.get(city, [])
 
 def get_data_columns(city):
+    """
+    Getting the data columns used for the given city.
+
+    Parameters:
+    - city: The city for which to retrieve data columns.
+
+    Returns:
+    - A list of data columns used for the city.
+    """
     return __data_columns.get(city, [])
 
+# If this script is run directly (not imported as a module)
 if __name__ == '__main__':
+    # Load the saved models and data columns
     load_saved_artifacts()
+    
+    # Printing the location names for 'bangalore' as a test
     print(get_location_names('bangalore'))
-    # print('Hii', get_data_columns('bangalore'))
-    # print(get_estimated_price('bangalore', '1st Phase JP Nagar', 1000, 3, 3))
-    # print(get_estimated_price('bangalore', '1st Phase JP Nagar', 1000, 2, 2))
-    # print(get_estimated_price('bangalore', 'Kalhalli', 1000, 2, 2)) # other location
-    # print(get_estimated_price('bangalore', 'Ejipura', 1000, 2, 2))  # other location
+    
